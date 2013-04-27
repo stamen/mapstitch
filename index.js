944a17b0ea141c9d1a973bb7535ac5130ebf1b65
Seth Fitzsimmons <seth@mojodna.net>
2013-04-26 22:13:27 -0700

    Custom extents, scaling, multiple tilesets, HTTP

--- index.js
+++ index.js
@@ -4,72 +4,153 @@ var http = require("http"),
     util = require("util");
 
 var async = require("async"),
+    cors = require("cors"),
     d3 = require("d3"),
+    express = require("express"),
+    app = express(),
     request = require("request"),
     Canvas = require("canvas"),
-    Image = Canvas.Image;
+    Image = Canvas.Image,
+    SphericalMercator = require("sphericalmercator"),
+    merc = new SphericalMercator({ size: 256 });
 
-http.globalAgent.maxSockets = 32;
+var SOURCES = require("./sources.json");
 
-var tiles = d3.range(653, 659).map(function(x) {
-  return d3.range(1581, 1585).map(function(y) {
-    return util.format("12/%d/%d.jpg", x, y);
-  });
+app.configure(function() {
+  app.use(express.responseTime());
+  app.use(express.logger());
+  app.use(cors());
+
+  http.globalAgent.maxSockets = 32;
 });
 
-var paths = tiles.reduce(function(a, b) {
-  return a.concat(b);
-}, []);
+app.get("/mapimg", function(req, res) {
+  console.log(req.query);
+  var zoom = 2,
+      bbox,
+      width = +req.query.w || 1500,
+      height = +req.query.h || 1000,
+      extent = req.query.extent.split(":", 4);
 
-var tasks = paths.map(function(x) {
-  return function(callback) {
-    request({
-      url: "http://tile.stamen.com/watercolor/" + x,
-      encoding: null
-    }, function(err, res, body) {
+  // rewrite extent in xy order
+  extent = [extent.slice(0, 2).reverse(), extent.slice(2, 4).reverse()];
+
+  // loop until the pixel delta is larger than the desired width and height
+  do {
+    zoom++;
+    bbox = [merc.px(extent[0], zoom), merc.px(extent[1], zoom)];
+  } while (bbox[1][0] - bbox[0][0] < width || bbox[1][1] - bbox[0][1] < height);
+
+  var tileRange = merc.xyz(merc.ll(bbox[0], zoom).concat(merc.ll(bbox[1], zoom)), zoom);
+
+  var tiles = d3.range(tileRange.minX, tileRange.maxX + 1).map(function(x) {
+    // tileRange.*Y is backward because our y coordinates start at the top
+    return d3.range(tileRange.maxY, tileRange.minY + 1).map(function(y) {
+      return {
+        zoom: zoom,
+        x: x,
+        y: y
+      };
+    });
+  });
+
+  var sourceUrl = SOURCES[req.query.p];
+
+  if (!sourceUrl) {
+    // no such source available
+    return res.send(404);
+  }
+
+  var urls = tiles.reduce(function(a, b) {
+    return a.concat(b);
+  }, []).map(function(x) {
+    return util.format(sourceUrl, x.zoom, x.x, x.y);
+  });
+
+  var tasks = urls.map(function(url) {
+    return function(callback) {
+      console.time(url);
+
+      request({
+        url: url,
+        encoding: null
+      }, function(err, res, body) {
+        console.timeEnd(url);
+
+        if (err) {
+          console.error(err);
+        }
+
+        if (res && res.statusCode === 200) {
+          var img = new Image();
+          img.src = body;
+
+          return callback(null, img);
+        } else if (res) {
+          console.log(res.statusCode);
+        }
+
+        return callback();
+      });
+    };
+  });
+
+  console.time("fetching tiles");
+
+  async.parallel(tasks, function(err, results) {
+    console.timeEnd("fetching tiles");
+
+    var rows = tiles.length,
+        cols = tiles[0].length;
+
+    console.time("stitching");
+
+    var canvas = new Canvas(tiles.length * 256, tiles[0].length * 256),
+        ctx = canvas.getContext("2d");
+
+    var x = 0,
+        y = 0;
+    results.forEach(function(img, i) {
+      if (img) {
+        ctx.drawImage(img, x, y, 256, 256);
+      }
+
+      if (i % cols === cols - 1) {
+        x += 256;
+        y = 0;
+      } else {
+        y += 256;
+      }
+    });
+
+    console.timeEnd("stitching");
+
+    console.time("scaling");
+
+    var target = new Canvas(width, height),
+        ctx = target.getContext("2d");
+
+    console.log("width: %d, height: %d", width, height);
+    ctx.drawImage(canvas, 0, 0, width, height);
+
+    console.timeEnd("scaling");
+
+    console.time("outputting");
+
+    target.toBuffer(function(err, buf) {
+      console.timeEnd("outputting");
+
       if (err) {
         console.error(err);
+        return res.send(500);
       }
 
-      if (res && res.statusCode === 200) {
-        var img = new Image();
-        img.src = body;
-
-        return callback(null, img);
-      } else if (res) {
-        console.log(res.statusCode);
-      }
-
-      return callback();
+      res.set("Content-Type", "image/png");
+      res.send(buf);
     });
-  };
+  });
 });
 
-async.parallel(tasks, function(err, results) {
-  var height = tiles.length,
-      width = tiles[0].length;
-
-  var canvas = new Canvas(tiles.length * 256, tiles[0].length * 256),
-      ctx = canvas.getContext("2d");
-
-  var x = 0,
-      y = 0;
-  results.forEach(function(img, i) {
-    if (img) {
-      ctx.drawImage(img, x, y, 256, 256);
-    }
-
-    if (i !== 0 && i % width === 0) {
-      x += 256;
-      y = 0;
-    } else {
-      y += 256;
-    }
-  });
-
-  var fs = require("fs"),
-      out = fs.createWriteStream(__dirname + "/out.png"),
-      stream = canvas.pngStream();
-
-  stream.pipe(out);
+app.listen(process.env.PORT || 8080, function() {
+  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
 });
